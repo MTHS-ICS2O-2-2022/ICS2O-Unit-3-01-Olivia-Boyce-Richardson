c0e344076beee7cb021a8bec653994cd31978c57
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-05 01:48:34 +0000

    html fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function calculate () {
+ // input
+ const aBase = parseInt(document.getElementById('a-base').value)
+ const bBase = parseInt(document.getElementById('b-base').value)
+ const height = parseInt(document.getElementById('height').value)
+
+ // process
+ const area = [ (aBase + bBase) / 2 ] * height
+
+ // output
+ document.getElementById('area').innerHTML = 'Area : ' + area.toFixed(2) + ' mm².'
+}
